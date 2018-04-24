8796cc0b931281b885183afaceacf8a579e95f69
ILABUTK <ilab.utk@gmail.com>
2018-04-24 17:24:50 -0400

    add a "how" menu

--- BE/NodeJS/myapp/app.js
+++ BE/NodeJS/myapp/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mysqlRouter = require('./routes/mysql');
+var mysqlRouter = require('./routes/mysql'); // xp
 
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mysql', mysqlRouter);
+app.use('/mysql', mysqlRouter); // xp
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
